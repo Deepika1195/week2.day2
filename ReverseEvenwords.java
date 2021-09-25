5f6fb8535e4622b4602678bbc530855601a7c98c
Deepika1195 <90104367+Deepika1195@users.noreply.github.com>
2021-09-25 11:34:08 +0530

    Add files via upload

---
+++ ReverseEvenwords.java
@@ -0,0 +1,28 @@
+package week2.day2;
+
+public class ReverseEvenwords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String test = "I am a software testing engineer in chennai";
+		String Reverse = "";
+		String arr[] = test.split(" ");
+		for (int i = 0; i < arr.length; i++) {
+			if ((i+1) % 2 == 0) {
+				char[] charArray = arr[i].toCharArray();
+				for (int j = charArray.length - 1; j >= 0; j--) {
+					Reverse = Reverse + charArray[j];
+				}
+				System.out.print(Reverse);
+				Reverse = "";
+			} else {
+				System.out.print(" " + arr[i] + " ");
+			}
+		}
+
+	}
+
+	}
+
+
